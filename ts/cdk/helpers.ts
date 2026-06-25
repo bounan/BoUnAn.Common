ff83fc6565c16c5fac1f709164f3e80b13c750cf
HRAshton <12210721+hrashton@users.noreply.github.com>
2026-06-25 20:49:32 +0200

    chore: Add Cdk helpers

---
+++ ts/cdk/helpers.ts
@@ -0,0 +1,25 @@
+﻿import * as cdk from 'aws-cdk-lib';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+
+export const getCfnValue = <Config extends Record<string, string>>(
+  key: keyof Config,
+  prefix: string,
+  exportSuffix: keyof Config & string,
+  overrides: Config,
+): string => {
+  return overrides[key] || cdk.Fn.importValue(prefix + exportSuffix);
+}
+
+export const getSsmValue = <Config extends Record<string, string>>(
+  stack: cdk.Stack,
+  parameterPrefix: string,
+  key: keyof Config & string,
+  overrides: Config,
+): string => {
+  return overrides[key] || ssm.StringParameter.valueForStringParameter(stack, parameterPrefix + key);
+}
+
+export const cdkOut = (stack: cdk.Stack, key: string, value: object | string): void => {
+  const output = typeof value === 'string' ? value : JSON.stringify(value);
+  new cdk.CfnOutput(stack, key, { value: output });
+}
