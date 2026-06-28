e7674964a6a23fb94d50d2c2f0b8c10cf37064a3
HRAshton <12210721+hrashton@users.noreply.github.com>
2026-06-28 17:12:28 +0200

    chore(ts): Add helper splitToChunks

---
+++ ts/runtime/collection-helpers.ts
@@ -0,0 +1,11 @@
+﻿/**
+ * Splits an array into chunks of a specified size.
+ * @param array The array to split.
+ * @param chunkSize The size of each chunk.
+ * @returns An array of chunks.
+ */
+export const splitToChunks = <T>(array: T[], chunkSize: number): T[][] =>
+  Array.from(
+    { length: Math.ceil(array.length / chunkSize) },
+    (_, i) => array.slice(i * chunkSize, (i + 1) * chunkSize),
+  );
